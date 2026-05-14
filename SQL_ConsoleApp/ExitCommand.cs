80c23e0a43963a6d2260ea3a1293b0e121a8aa04
URETZ <u-beresnev2006@mail.ru>
2026-05-15 03:00:11 +0700

    Первые 8 комманд

---
+++ SQL_ConsoleApp/ExitCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SQL_ConsoleApp
+{
+    internal class ExitCommand
+    {
+        static string EXIT_COMMAND = @"(?im)^\s*EXIT\s*;$";
+        private Match regex;
+        public ExitCommand(string command)
+        {
+            regex = Regex.Match(command, EXIT_COMMAND);
+            if (!regex.Success) { throw new Exception("Неверный синтаксис команды"); }
+        }
+    }
+}
---
+++ SQL_ConsoleApp/OpenCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SQL_ConsoleApp
+{
+    internal sealed class OpenCommand
+    {
+        static string OPEN_COMMAND = @"(?im)^\s*OPEN\s+(?<filePath>[^\s;]+)\s*;$";
+        private Match regex;
+        public OpenCommand(string command)
+        {
+            regex = Regex.Match(command, OPEN_COMMAND);
+            if (!regex.Success) { throw new Exception("Неверный синтаксис команды"); }
+        }
+        public string getPath()
+        {
+            return regex.Success ? regex.Groups["filePath"].Value:"";
+        }
+        public bool CheckPath()
+        {
+            if (!regex.Success) { return false; }
+            if (!regex.Groups["filePath"].Value.ToString().EndsWith(".dpf")) { return false; }
+            return true;
+        }
+    }
+}
